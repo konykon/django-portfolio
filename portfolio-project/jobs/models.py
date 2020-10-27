13be2662179848a35c83e43f4535adb3898efaaa
konykon <tsagarikon@hotmail.com>
2020-10-27 13:27:59 +0100

    add style y contact form

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
+    link = models.URLField(max_length=200, null=True)
--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 # Create your views here.
 def home(request): 
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+
+def contact(request):
+
+	if request.method == 'POST':
+
+		template = render_to_string('jobs/contact.html', {
+			'name':request.POST['name'],
+			'email':request.POST['email'],
+			'message':request.POST['message'],
+			})
+
+		email = EmailMessage(
+			request.POST['subject'],
+			template,
+			settings.EMAIL_HOST_USER,
+			['dennisivy11@gmail.com']
+			)
+
+		email.fail_silently=False
+		email.send()
+
+	return render(request, 'jobs/email_sent.html')
